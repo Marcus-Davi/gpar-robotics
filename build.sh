b3a44bbe3273b4e0410c479962553586a18efa49
projeto <davi2812@dee.ufc.br>
2020-06-30 20:15:07 -0300

    redesign

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+catkin_make -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --force-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_CXX_FLAGS="-std=c++14"
+
---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf ./build_isolated
+rm -rf ./devel_isolated
+rm -rf ./build
+rm -rf ./devel
+
+
+
+
