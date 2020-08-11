5eeebeeeede1affd33d1fd9e4399b8a2e92e20a2
Marcus Forte <davi2812@dee.ufc.br>
2020-08-10 21:19:22 -0300

    funciona coppelia + cena limpa + renomeado

--- src/gpar_hulk/scripts/gpar_hulk_go2goal.py
+++ src/gpar_hulk/scripts/gpar_hulk_go2goal.py
@@ -23,7 +23,8 @@ def go2goal(xgoal = 0, ygoal = 0, thetagoal = 0):
 
     #COPPELIA
     velocity_publisher = rospy.Publisher('/velocidade_hulk', Twist, queue_size=10)
-    vel_subscriber = rospy.Subscriber('/measured_velocity', Vector3, updatePose)
+    #vel_subscriber = rospy.Subscriber('/measured_velocity', Vector3, updatePose)
+    pose_subscriber = rospy.Subscriber('/measured_pose', Vector3, updatePose)
 
     #TARTARUGA DO ROS
     #velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10) 
@@ -37,85 +38,96 @@ def go2goal(xgoal = 0, ygoal = 0, thetagoal = 0):
 
     erro_acumulado = 0
     tol = 0.05
-    v = 1
+    v = 10
     w = 0
 
     #PID {
-    kp = 5
-    ki = 0.00000000000001
+    kp = 0.5
+    ki = 0.005
     #kd = 0
     #    }
+    rate = rospy.Rate(20)
 
-    while(v > tol):
-        errox = goal.x - position.x 
-        erroy = goal.y - position.y     
+    while not rospy.is_shutdown():
+        rate.sleep()
+
+        if(v > tol):
+
+
+
+            
+            errox = goal.x - position.x 
+            erroy = goal.y - position.y     
+            
+            ux = errox
+            uy = erroy
+
+            
+
+            theta_r = atan2(uy, ux)
+
+            v = sqrt(ux**2 + uy**2)
+            rospy.loginfo(v)
+            errot = theta_r - position.theta # erro do angulo theta
+
+            erro_acumulado += errot 
+
+            w = kp * errot + ki *  erro_acumulado #PI
+            vel = Twist()
+
+            if(v > 0.7):
+                v = 0.7
+            elif(v < -0.7):
+                v = -0.7
+
+
+
+            vel.linear.x = v
+            vel.linear.y = 0
+            vel.linear.z = 0
+
+            vel.angular.x = 0
+            vel.angular.y = 0
+            vel.angular.z = w
+
+            #rospy.loginfo(vel)
+            velocity_publisher.publish(vel)
+            rospy.logwarn(vel)
+
+            #updates
+            past_erro = errot
+
+        else :
+            rospy.logwarn('cabou!')
+            vel.linear.x = 0
+            vel.angular.z = 0
+            velocity_publisher.publish(vel)
+            return
         
-        ux = errox
-        uy = erroy
-
-        theta_r = atan2(uy, ux)
-
-        v = sqrt(ux**2 + uy**2)
-
-        errot = theta_r - position.theta # erro do angulo theta
-
-        erro_acumulado += errot 
-
-        w = kp * errot + ki *  erro_acumulado #PI
-        vel = Twist()
-
-        vel.linear.x = v
-        vel.linear.y = 0
-        vel.linear.z = 0
-
-        vel.angular.x = 0
-        vel.angular.y = 0
-        vel.angular.z = w
-
-        #rospy.loginfo(vel)
-        velocity_publisher.publish(vel)
-
-        #updates
-        past_erro = errot
-        
-    pose_subscriber.unregister()
-    return
+    #pose_subscriber.unregister()
+    
 
 
-
-#USO COM A TARTARUGA DO ROS
-def updatePose(pose):
-    '''
-    rospy.loginfo(pose)
-    position.x = pose.x
-    position.y = pose.y
-    position.theta = pose.theta
-'''
-
-#USO COM O SIMULADOR DO VREṔ
-def updatePose(weel_vel):
+#USO COM O SIMULADOR DO VREṔ & TURTLESIM
+def updatePose(pose): 
     #rospy.loginfo(weel_vel)
-    ve = weel_vel.x
-    vd = weel_vel.y
-    
-    v = r * (vd + ve)/2
-    w = (vd - ve)/l
-    
-    dt = 0.1
 
-    position.x = position.x + v*cos(position.theta) * dt
-    position.y = position.y + v*sin(position.theta) * dt
-    position.theta = position.theta + w*dt
-    position.theta = atan2(tan(position.theta), 1)
-    rospy.loginfo(position)
+    position.x = pose.x 
+    position.y = pose.y 
+    position.theta = pose.z
+    #rospy.loginfo(position)
  
 
 if __name__ == "__main__":
     try:
-        x = 5
-        y = 5
-        theta = pi/2
+
+
+        # Fazer lista
+        x = 3
+        y = 1
+        theta = 0
         go2goal(x,y,theta)
+        
 
     except rospy.ROSInterruptException :
         pass
