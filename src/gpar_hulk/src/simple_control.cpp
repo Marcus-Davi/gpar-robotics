7ba341a27263fa392632a9419b184167f8655369
William <williamsantostr@gmail.com>
2020-08-19 10:25:54 -0300

    code follow a trajectory

--- src/gpar_hulk/src/simple_control.cpp
+++ src/gpar_hulk/src/simple_control.cpp
@@ -2,18 +2,21 @@
 #include "geometry_msgs/Point.h"
 #include <cmath>
 #include <iostream>
+#include <vector>
 
 //Variáveis para realizar o controle
 geometry_msgs::Point velocidade;
 
-float u,v=1,w,Kp = 10,tol=0.01,errox,erroy,R=0.08,L=0.16,x,y,theta;
+int contador = 0;
+float u,v=1,w,Kp = 10,tol=0.1,errox=1,erroy=1,R=0.08,L=0.16,theta;
+std::vector<float> x{0,0,2,2,0},y{0,2,2,0,0};
 
 //Função que vai receber os dados da posição do Coppelia e fazer o cálculo de W e V
 void Dados_Posicao(const geometry_msgs::Point::ConstPtr& posicao_atual){
-	if(v>tol){
+	if((std::abs(errox)>tol || std::abs(erroy)>tol)){
 
-	errox = x - posicao_atual->x;
-	erroy = y - posicao_atual->y;
+	errox = x[contador] - posicao_atual->x;
+	erroy = y[contador] - posicao_atual->y;
 	
 	//Obs: em z foi colocado o theta, já que a posição Z não é utilizada.
 	theta = atan2(erroy,errox);
@@ -26,10 +29,19 @@ void Dados_Posicao(const geometry_msgs::Point::ConstPtr& posicao_atual){
 
 	}
 	else{
-	v = 0;
-	w=0;
+		v = 0;
+		
+		errox = x[contador] - posicao_atual->x;
+		erroy = y[contador] - posicao_atual->y;
+		
+		if(std::abs (errox)<=tol && std::abs(erroy)<=tol)
+			contador++;
+		
+		if(contador == 5)
+			contador = 0;	
+	
 }
-	ROS_INFO("Posicao X: %.2f\nPosicao Y: %.2f\nTheta: %.2f\nU: .%2f\nW: .%2f\nV: %.2f\nx= %.2f\ny = %.2f",posicao_atual->x,posicao_atual->y,posicao_atual->z,u,w,v,x,y);
+	ROS_INFO("Posicao X: %.2f\nPosicao Y: %.2f\nTheta: %.2f\nU: .%2f\nW: .%2f\nV: %.2f\nx= %.2f\ny = %.2f\nerrox = %.2f\nerroy = %.2f\n",posicao_atual->x,posicao_atual->y,posicao_atual->z,u,w,v,x[contador],y[contador],errox,erroy);
 
 }
 
@@ -43,10 +55,7 @@ int main(int argc, char **argv){
 	ros::Publisher pub_vel = n.advertise<geometry_msgs::Point>("/comando1",1000);
 	
 	ros::Rate r(20);
-	if ((n.getParam("X",x)) && (n.getParam("Y",y)))
-		std::cout<<"Parametros X e Y lidos com sucesso, x = "<<x<<" y = "<<y;
 	
-
 	while(ros::ok()){	
 		velocidade.x = (2*v-w*L)/(2*R); //Velocidade da Roda Esquerda
 		velocidade.y = (2*v+w*L)/(2*R); //Velocidade da Roda Direita
